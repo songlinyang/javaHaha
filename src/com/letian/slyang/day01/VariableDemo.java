1c7d8a715792e7a84e978484c4182c8072c831ae
slyang <376681881@qq.com>
2018-10-14 23:35:35 +0800

    java部署

---
+++ src/com/letian/slyang/day01/VariableDemo.java
@@ -0,0 +1,25 @@
+package com.letian.slyang.day01;
+
+public class VariableDemo {
+	public static void main(String[] args){
+		boolean aa = true;
+		byte bb = 1;
+		short cc = 32767;
+		int dd = 2147483647;
+		long ee = 100;
+//		浮点型
+		float ff = 3.14f; 
+		float ff1 = (float)3.14;
+		//字符型
+		char hh = 'a';
+		String kk = "adbc";
+		int [] arr1 = new int [4];
+		int a = 1;
+		int b =2;
+		for(int i:arr1){
+			System.out.println(i);
+			
+		}
+		System.out.println("java集成部署完成");
+	}
+}
